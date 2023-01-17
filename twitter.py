ca7c9943f6bff79d2eaa20e1274f45d71ad56ceb
SilasZhr <silaszhr@gmail.com>
2023-01-18 01:42:18 +0800

    fix: fix some errors & code format

--- twitter.py
+++ twitter.py
@@ -1,6 +1,6 @@
 import re
-import tweepy 
-import time 
+import tweepy
+import time
 import sqlite3
 import requests
 import json
@@ -23,100 +23,110 @@ client = tweepy.Client(
     consumer_key=consumer_key,
     consumer_secret=consumer_secret,
     access_token=access_token,
-    access_token_secret=access_token_secret
+    access_token_secret=access_token_secret,
 )
 
-links = ["https://twitter.com/DestroyPhish","https://twitter.com/TAKAMURASANG",
-        "https://twitter.com/AtomSpam",
-        "https://twitter.com/quicksandphish",
-        "https://twitter.com/kioan",
-        "https://twitter.com/ecarlesi",
-        "https://twitter.com/1c4m3by",
-        "https://twitter.com/PhishStats",
-        "https://twitter.com/CryptoPhishing"]
+links = [
+    "https://twitter.com/DestroyPhish",
+    "https://twitter.com/TAKAMURASANG",
+    "https://twitter.com/AtomSpam",
+    "https://twitter.com/quicksandphish",
+    "https://twitter.com/kioan",
+    "https://twitter.com/ecarlesi",
+    "https://twitter.com/1c4m3by",
+    "https://twitter.com/PhishStats",
+    "https://twitter.com/CryptoPhishing",
+]
 
 ids = []
 
 patterns = {
-            "https://twitter.com/DestroyPhish" : r'https?:\/\/([\w,-]*\[\.\])+\w+',
-            "https://twitter.com/AtomSpam": r'hxxps?:\/\/([\w,-]*\[\.\])+\w+',
-            "https://twitter.com/quicksandphish": r'hxxps?:\/\/([\w,-]*\[\.\])+\w+',
-            "https://twitter.com/kioan": r'https?\[:\]\/\/([\w,-]*[\[\.\], .])+\w+',
-            "https://twitter.com/ecarlesi": r'(hxxps?:\/\/)?([\w,-]*\[\.\])+\w+',
-            "https://twitter.com/1c4m3by": r'\/\/([\w,-]*[\[\.\], .])+\w+',
-            "https://twitter.com/PhishStats": r'https?:\/\/([\w,-]*\[\.\])+\w+',
-            "https://twitter.com/CryptoPhishing": r'(hxxps?:\/\/)?([\w,-]*\[\.\])+\w+'
+    "https://twitter.com/DestroyPhish": r"https?:\/\/([\w,-]*\[\.\])+\w+",
+    "https://twitter.com/AtomSpam": r"hxxps?:\/\/([\w,-]*\[\.\])+\w+",
+    "https://twitter.com/quicksandphish": r"hxxps?:\/\/([\w,-]*\[\.\])+\w+",
+    "https://twitter.com/kioan": r"https?\[:\]\/\/([\w,-]*[\[\.\], .])+\w+",
+    "https://twitter.com/ecarlesi": r"(hxxps?:\/\/)?([\w,-]*\[\.\])+\w+",
+    "https://twitter.com/1c4m3by": r"\/\/([\w,-]*[\[\.\], .])+\w+",
+    "https://twitter.com/PhishStats": r"https?:\/\/([\w,-]*\[\.\])+\w+",
+    "https://twitter.com/CryptoPhishing": r"(hxxps?:\/\/)?([\w,-]*\[\.\])+\w+",
+}
 
-        }
-
-#id = 989483566391160832
-#res = client.get_users_tweets(id=id, exclude=['retweets', 'replies'], max_results=5)
-
-
+'''
+convert hxxps://www[.]google[.].com -> https://www.google.com
+'''
 def clean_url(url: str):
-    url = url.replace('[', '')
-    url = url.replace(']', '')
-    url = url.replace('hxxp', 'http')
+    url = url.replace("[", "")
+    url = url.replace("]", "")
+    url = url.replace("hxxp", "http")
     if url.startswith("//"):
-        url = 'https' + url
+        url = "https:" + url
     return url
+
 
 def getIds():
     for link in links:
-        screenName = link.split('/')[-1]
-        id = getIdByName(screenName)
+        id = getIdByLink(link)
         ids.append(id)
+
 
 def getIdByName(name: str):
     res = client.get_user(username=name)
     id = res.data.id
     return id
 
+
 def getIdByLink(link: str):
-    name = link.split('/')[-1]
-    res = client.get_user(username=name)
-    id = res.data.id
+    name = link.split("/")[-1]
+    id = getIdByName(name)
     return id
 
+'''
+return an hour before now, which is  used as tweets start_time
+'''
 def getDate():
     timestamp = datetime.now(timezone.utc)
     addition = timedelta(hours=1)
     timestamp = timestamp - addition
     return timestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
 
+
 def fetch_and_store_data(link):
-    print("start",link)
+    print("start", link)
     id = getIdByLink(link)
-    screen_name = link.split('/')[-1]
+    screen_name = link.split("/")[-1]
     startTime = getDate()
     print(startTime)
     name = screen_name
     username = screen_name
     try:
-        response = client.get_users_tweets(id=id, exclude=['retweets', 'replies'], start_time=startTime)
+        response = client.get_users_tweets(
+            id=id, exclude=["retweets", "replies"], start_time=startTime
+        )
         tweets = response.data
-        #print(tweets)
+        # print(tweets)
         if not tweets:
             return
         for tweet in tweets:
-                    # print(tweet)
-                    status = tweet.id #tweet_id
-                    content = tweet.text
-                    if not content:
-                        continue
-                    for li in content.split("\n"):
-                        #print(li)
-                        res = re.search(patterns[link], li)
-                        if not res:
-                            continue
-                        result = res.group()
-                        lnk = clean_url(result)
-                        print(lnk)
-                        print(f"insert into main(tweet_id,username,name,images,tweet_url,link,profile) VALUES('{status}','{username}','{name}','{[]}','{link}','{lnk}','{link}')")
-                        cur.execute(
-                            f"insert into main(tweet_id,username,name,images,tweet_url,link,profile) VALUES('{status}','{username}','{name}','{[]}','{link}','{lnk}','{link}')")
-                        con.commit()
-
+            # print(tweet)
+            status = tweet.id  # tweet_id
+            content = tweet.text
+            if not content:
+                continue
+            for li in content.split("\n"):
+                # print(li)
+                res = re.search(patterns[link], li)
+                if not res:
+                    continue
+                result = res.group()
+                lnk = clean_url(result)
+                print(lnk)
+                print(
+                    f"insert into main(tweet_id,username,name,images,tweet_url,link,profile) VALUES('{status}','{username}','{name}','{[]}','{link}','{lnk}','{link}')"
+                )
+                cur.execute(
+                    f"insert into main(tweet_id,username,name,images,tweet_url,link,profile) VALUES('{status}','{username}','{name}','{[]}','{link}','{lnk}','{link}')"
+                )
+                con.commit()
 
     except Exception as ex:
         print("fetch_and_store_data", ex)
